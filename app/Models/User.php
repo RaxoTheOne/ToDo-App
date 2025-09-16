e98b5a4549aba91e699490c9ce1926c3c148e864
Benjamin Gayda-Knop <traxon.bg@gmail.com>
2025-09-16 12:07:58 +0200

    Füge eine Beziehung zwischen dem User- und dem Task-Modell hinzu, um die Zuordnung von Aufgaben zu Benutzern zu ermöglichen. Optimiere die Importanweisungen für bessere Lesbarkeit.

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,12 +1,12 @@
 <?php
-
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
@@ -45,5 +45,10 @@ class User extends Authenticatable implements MustVerifyEmail
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
     }
 }
