ddeb582738fcc962f62a45335ff52ff8e62fee26
Benjamin Gayda-Knop <traxon.bg@gmail.com>
2025-09-18 16:16:53 +0200

    Entferne die Implementierung von MustVerifyEmail aus dem User-Modell und ändere das Attribut 'email' in 'username', um die Authentifizierungslogik weiter zu vereinfachen und die Verwendung von Benutzernamen zu unterstützen.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,12 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -20,7 +19,7 @@ class User extends Authenticatable implements MustVerifyEmail
      */
     protected $fillable = [
         'name',
-        'email',
+        'username',
         'password',
     ];
 
@@ -42,7 +41,6 @@ class User extends Authenticatable implements MustVerifyEmail
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
